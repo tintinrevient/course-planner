e6e4be15feb1071f8aea26d8541747484f793c92
Zhao Shu <zhaobenben007@googlemail.com>
2019-10-16 23:21:54 +0200

    update trust function for multi agent network

--- course-planner/src/main/java/nl/uu/group8/courseplanner/CoursePlannerApplication.java
+++ course-planner/src/main/java/nl/uu/group8/courseplanner/CoursePlannerApplication.java
@@ -2,11 +2,13 @@ package nl.uu.group8.courseplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CoursePlannerApplication {
 
--- course-planner/src/main/java/nl/uu/group8/courseplanner/controller/CourseController.java
+++ course-planner/src/main/java/nl/uu/group8/courseplanner/controller/CourseController.java
@@ -2,16 +2,21 @@ package nl.uu.group8.courseplanner.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import nl.uu.group8.courseplanner.service.DLQueryEngine;
+import nl.uu.group8.courseplanner.util.BetaReputation;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.util.ShortFormProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.net.InetAddress;
 import java.util.*;
 import org.springframework.web.client.RestTemplate;
-
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
@@ -28,34 +33,103 @@ public class CourseController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    Environment environment;
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    private Map<String, List<Integer>> worldStates = new HashMap<>();
+    private Map<String, List<Boolean>> feedback = new HashMap<>();
+
     @GetMapping("/ask-eval")
     public int ask(@RequestParam String course) throws Exception {
-        ResponseEntity<Integer> responseEntity = restTemplate.getForEntity("http://145.107.86.180:8081/course/answer-eval?course="+course, Integer.class);
-        return responseEntity.getBody();
+
+        String localServerUrl = InetAddress.getLocalHost().getHostAddress() + ":" + environment.getProperty("local.server.port");
+        log.info("local server URL: " + localServerUrl);
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances(serviceId);
+
+        for(ServiceInstance serviceInstance : serviceInstanceList) {
+            String host = serviceInstance.getHost();
+            int port = serviceInstance.getPort();
+            String url = host + ":" + port;
+
+            if(null != url && url.equalsIgnoreCase(localServerUrl))
+                continue;
+
+            String serviceUrl = "http://" + url + "/course/answer-eval?course=" + course;
+            ResponseEntity<Integer> responseEntity = restTemplate.getForEntity(serviceUrl, Integer.class);
+
+            log.info("Ask the evaluation from " + url);
+
+            if(!worldStates.containsKey(url)) {
+                List<Integer> occurrences = new ArrayList<>();
+                occurrences.add(responseEntity.getBody());
+                worldStates.put(url, occurrences);
+
+                List<Boolean> feedbacks = new ArrayList<>();
+                feedbacks.add(new Random().nextBoolean());
+                feedback.put(url, feedbacks);
+
+            } else {
+                log.info("add to list ...");
+                worldStates.get(url).add(responseEntity.getBody());
+                feedback.get(url).add(new Random().nextBoolean());
+            }
+        }
+
+        Map<String, Double> rating = BetaReputation.reputationRating(feedback);
+
+        log.info("World state ...");
+        for(String url : worldStates.keySet())
+            log.info(url + ":" + worldStates.get(url));
+
+        log.info("Feedback ...");
+        for(String url : feedback.keySet())
+            log.info(url + ":" + feedback.get(url));
+
+        log.info("Rating ...");
+        for(String url : rating.keySet())
+            log.info(url + ":" + rating.get(url));
+
+        if(rating.keySet().size() == 0)
+            return 0;
+        else {
+            List<Integer> scoreList = worldStates.get(rating.entrySet().iterator().next().getKey());
+            return scoreList.get(scoreList.size()-1);
+        }
     }
 
     @GetMapping("/answer-eval")
     public int answer(HttpServletRequest request, @RequestParam String course) throws Exception {
-        log.info("Request coming from " + request.getRemoteAddr());
-        return 1;
+        StringBuffer requestURL = request.getRequestURL();
+        String requestURI = request.getRequestURI();
+        String url = requestURL.substring(0, requestURL.indexOf(requestURI));
+
+        log.info("Answer the evaluation from " + url);
+
+        return new Random().ints(1, 11).findFirst().getAsInt();
     }
 
     @PostMapping("/search")
     public ResponseEntity<?> search(@RequestBody String query) throws Exception {
 
-        List instanceList = new ArrayList<String>();
+        List<String> instanceList = new ArrayList<>();
         Set<OWLNamedIndividual> individuals = engine.getInstances(query, false);
         for (OWLEntity entity : individuals) {
             instanceList.add(shortFormProvider.getShortForm(entity));
         }
 
-        List superClassList = new ArrayList<String>();
+        List<String> superClassList = new ArrayList<>();
         Set<OWLClass> superClasses = engine.getSuperClasses(query, false);
         for (OWLClass class_ : superClasses) {
             superClassList.add(shortFormProvider.getShortForm(class_));
         }
 
-        List subClassList = new ArrayList<String>();
+        List<String> subClassList = new ArrayList<>();
         Set<OWLClass> subClasses = engine.getSubClasses(query, false);
         for (OWLClass class_ : subClasses) {
             subClassList.add(shortFormProvider.getShortForm(class_));
